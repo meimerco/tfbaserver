03218db80947ce6ee5b83584966ad210c5dd6144
kiki <daxiaojie11@gmail.com>
2020-04-11 19:48:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const hbs = require("nodemailer-express-handlebars");
+require("dotenv").config();
+
+const app = express();
+
+const transporter = nodemailer.createTransport({
+  service: process.env.EMAIL_SERVER,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const handlebarOptions = {
+  viewEngine: {
+    extName: ".hbs",
+    partialsDir: "./views/",
+    layoutsDir: "./views/",
+    defaultLayout: "layout.hbs",
+  },
+  viewPath: "./views/",
+  extName: ".hbs",
+};
+
+transporter.use("compile", hbs(handlebarOptions));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000" || process.env.CLIENT_ORIGIN,
+    credentials: true,
+  })
+);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("server");
+});
+
+app.get("/api/contactForm", (req, res) => {
+  res.send("send contact form");
+});
+
+app.post("/api/contactForm", (req, res) => {
+  const data = req.body;
+  // const html = `<div>Name: ${data.name}</div><div>Email:  ${data.email}</div><div>Subject: ${data.subject}</div><div><p>Message:</p> ${data.message}</div>`;
+  var mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: process.env.CONTACT_FORM_SEND_TO,
+    cc: process.env.CONTACT_FORM_CC,
+    subject: "You've got a contact request from TacticalFBA",
+    // html: html,
+    template: "contactformreply",
+    context: { data },
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+  res.send("contact form sent");
+});
+
+app.get("/api/orderEmail", (req, res) => {
+  res.send("order email server");
+});
+
+app.post("/api/orderEmail", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  var mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: data.info.user,
+    cc: process.env.CONTACT_FORM_CC,
+    subject: `TacticalFBA Order Confirmation`,
+    text: data.info.date,
+    template: "orderEmail",
+    context: { data },
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+  res.send("order email sent");
+});
+
+const port = 4000 || process.env.PORT;
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
