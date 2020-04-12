8055675d71ddf824339a8777a2db7b49511653a8
kiki <daxiaojie11@gmail.com>
2020-04-12 11:31:12 +0800

    changed port

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: { rejectUnauthorized: false },
 });
 
 const handlebarOptions = {
