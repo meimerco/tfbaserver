1e9dae80f33c27803ec397035a308313b4e0d5fe
kiki <daxiaojie11@gmail.com>
2020-04-12 11:27:11 +0800

    changed port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const transporter = nodemailer.createTransport({
   host: "smtp.qq.com",
   secureConnection: true,
-  port: 465,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.EMAIL_USERNAME,
