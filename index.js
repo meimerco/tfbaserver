5dcc72e6129f6686b49af4d04b6e415ae4b857e1
kiki <daxiaojie11@gmail.com>
2020-04-13 12:54:33 +0800

    req body tr4

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-res.header("Content-Type", "application/json; charset=utf-8");
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+  console.log(123);
+});
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
