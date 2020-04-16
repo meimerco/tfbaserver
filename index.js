0888e8debb25859381cd4a5fbcdb1b4d3131ce17
kiki <daxiaojie11@gmail.com>
2020-04-16 12:33:14 +0800

    hopefully last commit :p

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-// const hbs = require("nodemailer-express-handlebars");
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 
@@ -20,20 +18,6 @@ app.use(
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// const handlebarOptions = {
-//   viewEngine: {
-//     extName: ".hbs",
-//     partialsDir: "./views/",
-//     layoutsDir: "./views/",
-//     defaultLayout: "layout.hbs",
-//   },
-//   viewPath: "./views/",
-//   extName: ".hbs",
-// };
-
-// transporter.use("compile", hbs(handlebarOptions));
-// ("https://tacticalfba.netlify.com/");
-
 app.get("/", (req, res) => {
   res.send("TacticalFBA");
 });
@@ -44,7 +28,7 @@ app.post("/api/contactForm", (req, res) => {
   const msg = {
     to: process.env.CONTACT_FORM_SEND_TO,
     from: process.env.EMAIL_USERNAME,
-    // cc: process.env.CONTACT_FORM_CC,
+    cc: process.env.CONTACT_FORM_CC,
     subject: "You've got a contact request from TacticalFBA",
     text: `Name: ${data.name}\nEmail: ${data.email}\nSubject: ${data.subject}\nMessage: ${data.message}`,
   };
@@ -64,7 +48,6 @@ app.post("/api/orderEmail", (req, res) => {
   const msg = {
     to: data.info.user,
     from: process.env.EMAIL_USERNAME,
-    // cc: process.env.CONTACT_FORM_CC,
     template_id: "d-428e70d4b7544649b3499499ab220c06",
     dynamic_template_data: data,
   };
