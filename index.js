aec8c39941045ec94d9167f78634703d2b4b9994
Kiki <52940074+kikiren@users.noreply.github.com>
2020-10-13 11:31:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: new URL("https://www.tacticalfba.com/"),
+    origin: new URL("https://tfba.netlify.app/"),
     credentials: true,
   })
 );
